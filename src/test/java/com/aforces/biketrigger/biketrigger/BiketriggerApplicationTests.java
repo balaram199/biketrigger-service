387a72733a8c43caf2622a3b0549b6f9e3460a8a
Balaram swain <balaram.swain@unisys.com>
2019-02-28 22:55:40 +0530

    inital project setup

---
+++ src/test/java/com/aforces/biketrigger/biketrigger/BiketriggerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aforces.biketrigger.biketrigger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BiketriggerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
