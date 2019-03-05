d7a10cb9392ddea3b22ad540f4328a21ac9c3736
Balaram swain <balaram.swain@unisys.com>
2019-03-06 00:03:43 +0530

    data and schema.sql are added

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,55 @@
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` integer(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phno` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `role_id` integer(11),
+  `verified_on` DATETIME DEFAULT NULL,
+  `created_by` varchar(255) NOT NULL,
+  `created_on` DATETIME NOT NULL,
+  `update_by` varchar(255) NOT NULL,
+  `update_on` DATETIME NOT NULL
+)
+
+
+CREATE TABLE user_role (
+  `id` integer(11) NOT NULL,
+  `role_id` integer(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+)
+
+CREATE TABLE address (
+  `id` integer(11) NOT NULL,
+  `addressLine1` varchar(255) NOT NULL,
+  `addressLine2` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state`` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `pin` integer(6) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS `cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `state_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47577 ;
+
+
+CREATE TABLE IF NOT EXISTS `states` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `country_id` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4121 ;
+
+
+CREATE TABLE IF NOT EXISTS `countries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sortname` varchar(3) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `phonecode` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=249 ;
